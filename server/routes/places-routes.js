f9167544b277e9ccf6b3131b72dd5e9d616d0930
rickytorrez <torrez.ricky@ymail.com>
2020-11-06 10:04:09 -0500

    http-error models

--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -1,10 +1,57 @@
 const express = require("express");
 
+const HttpError = require("../models/http-error");
+
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET Request in Places");
-  res.json({ message: "it worked!" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1"
+  }
+];
+
+// @route   GET api/places/:id
+// @desc    gets a single place by its id
+// @access  Public
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404);
+  }
+
+  res.json({ place }); // { place } => { place === place }
+});
+
+// @route   GET api/places/user/:uid
+// @desc    gets places that belong to a single user by the user id
+// @access  Public
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find(p => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
+  }
+
+  res.json({ place });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -5,6 +5,17 @@ const placesRoutes = require("./routes/places-routes");
 
 const app = express();
 
-app.use(placesRoutes);
+// => /api/places
+app.use("/api/places", placesRoutes);
+
+app.use((error, req, res, next) => {
+  // headersSent -> check if the response has already been sent
+  if (res.headersSent) {
+    // return next and forward the error since we already sent the response
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error ocurred!" });
+});
 
 app.listen(5000);
